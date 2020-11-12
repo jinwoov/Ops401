841b3e150fd989ccf22a5ef8e3919289ed7de7da
jinfluenza <jinwoov@gmail.com>
2020-11-11 20:47:11 -0800

    done with ops

--- ops_challenge/ops-challenge28/classes/logs.py
+++ ops_challenge/ops-challenge28/classes/logs.py
@@ -3,18 +3,28 @@ import logging.config
 class Logs:
 
     def __init__(self, fname, lvls):
+        self.logger = logging.getLogger(__name__)
+        self.c_handler = logging.StreamHandler()
         self.f_handler = logging.FileHandler(fname)
         self.setting_lvl(lvls)
         self.format_log()
 
     def setting_lvl(self, lvl):
         if(lvl == "i"):
+            self.c_handler.setLevel(logging.INFO)
             self.f_handler.setLevel(logging.INFO)
         elif(lvl == "w"):
+            self.c_handler.setLevel(logging.WARNING)
             self.f_handler.setLevel(logging.WARNING)
         elif(lvl == "e"):
+            self.c_handler.setLevel(logging.ERROR)
             self.f_handler.setLevel(logging.ERROR)
 
     def format_log(self):
         f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
         self.f_handler.setFormatter(f_format)
+        self.c_handler.setFormatter(f_format)
+        self.logger.addHandler(self.f_handler)
+        self.logger.addHandler(self.c_handler)
+
+        
--- ops_challenge/ops-challenge28/main.py
+++ ops_challenge/ops-challenge28/main.py
@@ -1,19 +1,19 @@
-# Script Name: Ops Challenge 27
+# Script Name: Ops Challenge 28
 # Author: Jin Kim
-# Date of last revision: 11/190/2020
-# Description of purpose: Logging any error that was raised by recent function call part 2.
+# Date of last revision: 11/11/2020
+# Description of purpose: Logging any error that was raised by recent function call part 3
 
 # Import library
 import logging
 from classes.rotating_log import *
 from classes.rotating_time import *
+from classes.logs import *
 
 # Declare variables
 logging.basicConfig(filename="./log.log", level=logging.ERROR, format='%(asctime)s:%(levelname)s:%(message)s')
 logging.basicConfig(level=logging.INFO, format='%(asctime)s:%(levelname)s:%(message)s')
 log_file= "test.log"
 time_log="timed_test.log"
-
 # Declare functions
 ## Interface that will collect all of the functions into a menu button.
 def interface():
@@ -26,6 +26,9 @@ def interface():
     
     if (userInput == "1"):
         user_size = taking_inputs("What size do you want to save for each file? ")
+        if(~user_size.isdigit()):
+            log_warning = Logs("./warning.log", "w")
+            log_warning.logger.warning("Not a digit")
         rotating_log(log_file, user_size)
     elif (userInput == "2"):
         rotating_time(log_file)
@@ -44,7 +47,9 @@ def taking_inputs(stg):
 ## try/catch block
 try:
     interface()
+
 except Exception as msg:
-    logging.error(msg)
+    log_error = Logs("./error.log", "e")
+    log_error.logger.error(msg)
     
 # END
