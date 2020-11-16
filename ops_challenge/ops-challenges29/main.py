5ae3c85879983488f6bb0c01f8c0d00ef83a9f5a
jinfluenza <jinwoov@gmail.com>
2020-11-16 15:21:53 -0800

    comments are done

--- ops_challenge/ops-challenges29/main.py
+++ ops_challenge/ops-challenges29/main.py
@@ -1,7 +1,14 @@
+# Script Name: Ops Challenge 29
+# Author: Jin Kim
+# Date of last revision: 11/16/2020
+# Description of purpose: Finding the file using user input part 1/3
+
+
+# importing library
 import sys
 from classes.search import *
 
-
+## Declare functions
 def interface():
     # operSys = sys.platform
     # if operSys.lower().__contains__("win"):
@@ -11,9 +18,10 @@ def interface():
     search_file()
     exit(0)
 
-
+## Main
 def main():
     interface()
 
 if __name__ == "__main__":
     main()
+## END
