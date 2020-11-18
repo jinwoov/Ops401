5bd89db6f4fa1ce6e281b77792a8446b7903da66
Jin Kim <jinwoov@gmail.com>
2020-11-18 17:20:35 -0600

    done with comments

--- ops_challenge/ops-challenges33/classes/virustotal.py
+++ ops_challenge/ops-challenges33/classes/virustotal.py
@@ -1,16 +1,22 @@
 import os, getpass
 from decouple import config
 from .hashes import *
+
+## querying the virus total using virustotal-search.py and api
 def query_virusTotal():
+    ## getting the hash code of the file of choice
     hashcode = get_hashCode()
     path = os.path.dirname(__file__)
     api = input("Whats your API key? ")
+    ## validation to check if the key is there
     if(api == ""):
         api = config('vtkey')
     username = getpass.getuser()
+    ## calling the query
     query = f"python {path}/virustotal-search.py -k " + api + " -m " + hashcode
     os.system(query)
 
+## getting the hashcode using the previous challenge
 def get_hashCode():
     target_file = input("whats the target file? ")
     target_file = os.path.abspath(target_file)
