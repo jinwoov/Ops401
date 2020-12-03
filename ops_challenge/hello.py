99974add3301a547bd3ee16b4d560227ee68e264
jinfluenza <jinwoov@gmail.com>
2020-12-03 14:54:39 -0800

    finisehd all of the assignment

--- ops_challenge/hello.py
+++ ops_challenge/hello.py
@@ -1,18 +1,5 @@
-import subprocess
+h = "h l e"
 
-# nmap -sV 10.0.2.4
-ip_address = input("what ip/url do you want to scan ")
-port = input("what port do you want to scan ")
+nh = h.replace(h[-1], "l")
 
-process = subprocess.Popen(["nmap", "-sV", ip_address])
-try:
-    process.wait(timeout=3)
-except:
-    process.kill()
-
-# "telnet ip port"
-process1 = subprocess.Popen(["telnet", ip_address, port])
-try:
-    process1.wait(timeout=3)
-except:
-    process1.kill()
+print(h, nh)
